b484a805b511c7ead86297a1224be276e0b63cf0
David Alpert <david@spinthemoose.com>
2021-12-11 23:56:12 -0600

    day 03 part 1

--- 2021/Tests.fs
+++ 2021/Tests.fs
@@ -5,16 +5,23 @@ open System
 open Xunit
 open FsUnit.Xunit
 
-let soundingsFromInput (input:string) =
+let splitToTrimmedLines (input:string) =
     input.Trim().Split("\n")
     |> Seq.map (fun s -> s.Trim())
+
+let soundingsFromInput (input:string) =
+    splitToTrimmedLines input
     |> Seq.map (fun s -> s |> int)
 
 let submarineInstructionsFromInput (input:string) =
-    input.Trim().Split("\n")
-    |> Seq.map (fun s -> s.Trim())
+    splitToTrimmedLines input
     |> Seq.map (fun s -> s.Split " ")
     |> Seq.map List.ofArray
+
+let diagnosticReportFromInput (input:string) =
+    splitToTrimmedLines input
+    |> Seq.map List.ofSeq
+    |> List.ofSeq
 
 let depthIncreased (prev,next) =
     next > prev
@@ -24,6 +31,12 @@ let numberOfTimesDepthIncreases (soundings:int seq) =
     |> Seq.pairwise
     |> Seq.where depthIncreased
     |> Seq.length
+
+// https://rolfsuter.ch/code/f-transpose-a-list-of-lists-transpose-2d-matrix/
+let rec transpose = function
+    | (_::_)::_ as M -> 
+        List.map List.head M :: transpose (List.map List.tail M)
+    | _ -> []
 
 [<Fact>]
 let ``Day 01 - Part 1 - Example`` () =
@@ -121,3 +134,42 @@ let ``Day 02 - Part 2 - Calculation`` () =
     |> Seq.fold navigateSubmarineWithAim initialPosition
     |> (fun p -> p.displacement)
     |> should equal 1_840_311_528
+
+let mostCommonBit (col:char list) =
+    col
+    |> List.groupBy (fun c -> c)
+    |> List.map (fun (bit, bits) -> (bit, bits |> List.length))
+    |> List.sortBy (fun (_, n) -> n)
+    |> List.head
+    |> fst
+
+let epsilonFromGamma (gamma:string) =
+    gamma.ToCharArray()
+    |> Array.map (fun bit -> match bit with | '0' -> '1' | '1' -> '0' | _ -> ' ')
+    |> String
+
+let powerConsumption gamma =
+    let epsilon = gamma |> epsilonFromGamma
+    Convert.ToInt64(gamma, 2) * Convert.ToInt64(epsilon, 2)
+
+[<Fact>]
+let ``Day 03 - Part 1 - Example`` () =
+    Input.day03sample
+    |> diagnosticReportFromInput
+    |> transpose
+    |> List.map mostCommonBit
+    |> Array.ofList
+    |> String
+    |> powerConsumption
+    |> should equal (198 |> int64)
+
+[<Fact>]
+let ``Day 03 - Part 1 - Calculation`` () =
+    Input.day03data
+    |> diagnosticReportFromInput
+    |> transpose
+    |> List.map mostCommonBit
+    |> Array.ofList
+    |> String
+    |> powerConsumption
+    |> should equal (1458194 |> int64)
