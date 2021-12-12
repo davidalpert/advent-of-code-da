c9f053baafac6f3df7b804bf87e2dc3820e4a1d9
David Alpert <david@spinthemoose.com>
2021-12-11 23:13:39 -0600

    day 02 part 1

--- 2021/Tests.fs
+++ 2021/Tests.fs
@@ -10,6 +10,12 @@ let soundingsFromInput (input:string) =
     |> Seq.map (fun s -> s.Trim())
     |> Seq.map (fun s -> s |> int)
 
+let submarineInstructionsFromInput (input:string) =
+    input.Trim().Split("\n")
+    |> Seq.map (fun s -> s.Trim())
+    |> Seq.map (fun s -> s.Split " ")
+    |> Seq.map List.ofArray
+
 let depthIncreased (prev,next) =
     next > prev
 
@@ -51,3 +57,38 @@ let ``Day 01 - Part 2 - Calculation`` () =
     |> numberOfTimesDepthIncreases
     |> should equal 1150
 
+type SubmarinePosition = {
+    horizontal : int
+    depth : int
+}
+with
+    member x.displacement =
+        x.horizontal * x.depth
+
+let initialPosition = {
+    horizontal = 0;
+    depth = 0;
+}
+
+let navigateSubmarine (startingPosition:SubmarinePosition) (instructions:string list) =
+    match instructions with
+    | "forward" :: [x] -> { startingPosition with horizontal = startingPosition.horizontal + (x |> int) }
+    | "up"      :: [x] -> { startingPosition with depth = startingPosition.depth - (x |> int) }
+    | "down"    :: [x] -> { startingPosition with depth = startingPosition.depth + (x |> int) }
+    | _ -> startingPosition
+
+[<Fact>]
+let ``Day 02 - Part 1 - Example`` () =
+    Input.day02sample
+    |> submarineInstructionsFromInput
+    |> Seq.fold navigateSubmarine initialPosition
+    |> (fun p -> p.displacement)
+    |> should equal 150
+
+[<Fact>]
+let ``Day 02 - Part 1 - Calculation`` () =
+    Input.day02
+    |> submarineInstructionsFromInput
+    |> Seq.fold navigateSubmarine initialPosition
+    |> (fun p -> p.displacement)
+    |> should equal 2073315
