1fb5784227faa59f131e2f009760451ce30ad65f
David Alpert <david@spinthemoose.com>
2021-12-18 13:35:19 -0600

    day 07 - part 2

--- 2021/day07_tests.fs
+++ 2021/day07_tests.fs
@@ -3,13 +3,16 @@ namespace AdventOfCode.Tests
 module Day07 =
 
   open AdventOfCode.Input
-  open AdventOfCode.Crabs
   open Xunit
   open FsUnit.Xunit
 
-  let costToMoveTo (startingPositions:int[]) (x:int) =
+  // Each change of 1 step in horizontal position of a single crab costs 1 fuel. 
+  let part1FuelCost (finalPosition:int) (initialPosition:int) =
+    abs (initialPosition - finalPosition)
+
+  let costToMoveTo (startingPositions:int[]) (fuelCoster:int -> int -> int) (x:int) =
     startingPositions
-    |> Array.map (fun i -> abs (i - x) )
+    |> Array.map (fuelCoster x)
     |> Array.sum
 
   [<Theory>]
@@ -35,15 +38,15 @@ module Day07 =
       day07sample.Trim().Split(",")
       |> Array.map int
 
-    costToMoveTo m n
+    costToMoveTo m part1FuelCost n
     |> should equal cost
 
-  let minCostBySeq (m:int[]) =
+  let minCostBySeq (m:int[]) (fuelCoster:int -> int -> int) =
     let max = m |> Array.max
     let min = m |> Array.min
 
     seq { min .. max }
-    |> Seq.map (costToMoveTo m)
+    |> Seq.map (costToMoveTo m fuelCoster)
     |> Seq.min
 
   [<Fact>]
@@ -52,7 +55,7 @@ module Day07 =
       day07sample.Trim().Split(",")
       |> Array.map int
 
-    minCostBySeq m
+    minCostBySeq m part1FuelCost
     |> should equal 37
 
   [<Fact>]
@@ -61,5 +64,44 @@ module Day07 =
       day07data.Trim().Split(",")
       |> Array.map int
 
-    minCostBySeq m
+    minCostBySeq m part1FuelCost
     |> should equal 355764
+
+  // As it turns out, crab submarine engines don't burn fuel at a constant rate. Instead, each change of 1 step in horizontal position costs 1 more unit of fuel than the last: the first step costs 1, the second step costs 2, the third step costs 3, and so on.
+  let part2FuelCost (finalPosition:int) (initialPosition:int) =
+    let s = sign (finalPosition - initialPosition)
+    match s with
+    | 0 -> 0
+    | _ -> seq { initialPosition .. s .. finalPosition }
+           |> Seq.map (fun p -> abs (p - initialPosition))
+           |> Seq.sum
+    
+  [<Theory>]
+  [<InlineData(16,5,66)>]
+  [<InlineData(1,5,10)>]
+  [<InlineData(2,5,6)>]
+  [<InlineData(0,5,15)>]
+  [<InlineData(4,5,1)>]
+  [<InlineData(7,5,3)>]
+  [<InlineData(14,5,45)>]
+  let ``Day 07 - compound costToMoveTo`` (x1:int, x2:int, expectedCost:int) =
+    part2FuelCost x1 x2
+    |> should equal expectedCost
+    
+  [<Fact>]
+  let ``Day 07 - part 2 - example`` () =
+    let m =
+      day07sample.Trim().Split(",")
+      |> Array.map int
+
+    minCostBySeq m part2FuelCost
+    |> should equal 168
+
+  [<Fact(Skip="not an efficient implementation")>]
+  let ``Day 07 - part 2 - calculation`` () =
+    let m =
+      day07data.Trim().Split(",")
+      |> Array.map int
+
+    minCostBySeq m part2FuelCost
+    |> should equal 99634572
