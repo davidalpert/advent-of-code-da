701a701d8263592362a1829e417d2ce453cdd653
David Alpert <david.alpert@nfl.com>
2021-12-20 20:12:46 -0600

    day 10 - part 1 - parse chunks and lines

--- 2021/day10_parser.fs
+++ 2021/day10_parser.fs
@@ -14,14 +14,69 @@ module NavigationModel =
     |> dict
 
   type Chunk =
-  | ValidChunk of startingChar:char * children:Chunk[] * endingChar:char
-  | CorruptedChunk of startingChar:char * children:Chunk[] * expectedChar:char * actualChar:char
-  | IncompleteChunk of startingChar:char * children:Chunk[]
+  | ValidChunk of startingChar:char * children:Chunk seq * endingChar:char
+  | CorruptedChunk of startingChar:char * children:Chunk seq * expectedChar:char * actualChar:char * unparsed:string
+  | IncompleteChunk of startingChar:char * children:Chunk seq
+  with
+    member x.toAnnotatedString =
+      let ss (cc:Chunk seq) =
+        cc
+        |> Seq.map (fun c -> c.toAnnotatedString)
+        |> String.concat ""
+
+      match x with
+      | ValidChunk(s,cc,e)         -> sprintf "v:%c%s%c" s (ss cc) e
+      | CorruptedChunk(s,cc,e,a,u) ->
+        let p = sprintf "c:%c%s%c expected '%c' but got '%c'%s" s (ss cc) a e a
+        match u.Length with
+        | 0 -> p ""
+        | _ -> p (sprintf " ignoring: '%s'" u)
+      | IncompleteChunk(s,cc)      ->
+        sprintf "i:%c%s" s (ss cc)
+
+    static member tryFindCorrupted (cc:Chunk seq) =
+      let x =
+        cc
+        |> Seq.map (fun c -> c.tryFindCorrupt)
+        |> Seq.filter (fun (r:Chunk option) -> r.IsSome)
+
+      match x |> Seq.tryHead with
+      | Some(h) -> h
+      | None    -> None
+
+    member x.tryFindCorrupt =
+      match x with
+      | ValidChunk(_,cc,_)        -> Chunk.tryFindCorrupted cc
+      | CorruptedChunk(_,_,_,_,_) -> Some(x)
+      | IncompleteChunk(_,cc)     -> Chunk.tryFindCorrupted cc
 
   and Line =
-  | ValidLine of children:Chunk[]
-  | CorruptedLine of children:Chunk[] * corrruptChunk:Chunk
-  | IncompleteLine of children:Chunk[] * incompleteChunk:Chunk
+  | ValidLine of children:Chunk seq
+  | CorruptedLine of children:Chunk seq * corrruptChunk:Chunk
+  | IncompleteLine of children:Chunk seq * incompleteChunk:Chunk
+  with
+    member x.toAnnotatedString =
+      match x with
+      | ValidLine(cc)         -> sprintf "Valid      | %s" (cc |> Seq.map (fun c -> c.toAnnotatedString) |> String.concat " | ")
+      | CorruptedLine(cc, x)  -> sprintf "Corrupted  | %s" (cc |> Seq.map (fun c -> c.toAnnotatedString) |> String.concat " | ")
+      | IncompleteLine(cc, x) -> sprintf "Incomplete | %s" (cc |> Seq.map (fun c -> c.toAnnotatedString) |> String.concat " | ")
+
+    static member lift (chunks:Chunk seq) =
+      // printfn "lifting chunks: %s" (chunks |> Seq.map (fun c -> c.toAnnotatedString) |> String.concat "; ")
+
+      let allValid (cc:Chunk seq) =
+        cc |> Seq.forall  (fun c -> match c with ValidChunk(_,_,_) -> true | _ -> false)
+
+      let tryFindIncomplete (cc:Chunk seq) =
+        cc |> Seq.tryFind (fun c -> match c with IncompleteChunk(_,_) -> true | _ -> true)
+
+      match allValid chunks with
+      | true  -> ValidLine(chunks)
+      | false -> match Chunk.tryFindCorrupted chunks with
+                 | Some(c) -> CorruptedLine(chunks, c)
+                 | None -> match tryFindIncomplete chunks with
+                           | Some(c) -> IncompleteLine(chunks, c)
+                           | None -> failwith (sprintf "something went wrong %s" (chunks |> Seq.map (fun x -> x.toAnnotatedString) |> String.concat " | "))
 
 module NavigationParser =
 
@@ -30,10 +85,76 @@ module NavigationParser =
   open FParsec.Pipes
   open NavigationModel
 
+  let ch = pchar
+  let ws = spaces
+  // let nl = pchar '\n'
+
+  let (<!>) (p: Parser<_,_>) (label,startingChar,endingChar) : Parser<_,_> =
+    fun stream ->
+        // printfn "%A: Entering %s %c..%c" stream.Position label startingChar endingChar
+        let reply = p stream
+        // printfn "%A: Leaving %s %c..%c (%A)" stream.Position label startingChar endingChar reply.Status
+        reply
+
+  let rec pAnyValidChunk : Parser<Chunk,unit> =
+    choice (
+      validDelimeters
+      |> Seq.map (fun pair ->
+        (
+          %% +.(ch pair.Key) -- +.(%[pAnyValidChunk]*qty.[0..]) -? +.(ch pair.Value)
+          -|> fun s c e -> ValidChunk(s,c,e)
+        ) <!> ("pValidChunk",pair.Key,pair.Value)
+      )
+    )
+
+  and pAnyIncompleteChunk : Parser<Chunk,unit> =
+    choice (
+      validDelimeters
+      |> Seq.map (fun pair ->
+        (
+          %% +.(ch pair.Key) -- +.(%[pChunk]*qty.[0..])
+          -|> fun s c -> IncompleteChunk(s,c)
+        )
+      )
+    )
+
+  and pOneCorruptedChunk (startingChar:char): Parser<Chunk,unit> =
+    let expectedEndingChar = validDelimeters.[startingChar]
+
+    choice (
+      validDelimeters
+      |> Seq.filter (fun pair -> pair.Key <> startingChar)
+      |> Seq.map (fun pair -> 
+        (
+          %% (ch startingChar) -- +.(%[pAnyValidChunk]*qty.[0..]) -? (ch pair.Value) -- +.(restOfLine true)
+          -|> fun c rest -> CorruptedChunk(startingChar,c,expectedEndingChar,pair.Value, rest)
+        ) <!> ("pOneCorruptedChunk",startingChar,pair.Value)
+      )
+    )
+
+  and pAnyCorruptedChunk : Parser<Chunk,unit> =
+    choice (
+      validDelimeters
+      |> Seq.map (fun pair -> (pOneCorruptedChunk pair.Key))
+    )
+
+  and pChunk : Parser<Chunk,unit> =
+    choice [
+      pAnyValidChunk;
+      pAnyCorruptedChunk;
+      pAnyIncompleteChunk;
+    ]
+
   let pLine : Parser<Line,unit> =
-    preturn (ValidLine([||]))
+    %% ws -- +.(pChunk * qty.[1..])
+    -|> Line.lift
 
   let parseChunk (input:string) =
+    match run pChunk input with
+    | Success(r, _, _)   -> Result.Ok(r)
+    | Failure(errorMsg, _, _) -> Result.Error(errorMsg)
+
+  let parseLine (input:string) =
     match run pLine input with
     | Success(r, _, _)   -> Result.Ok(r)
     | Failure(errorMsg, _, _) -> Result.Error(errorMsg)
--- 2021/day10_tests.fs
+++ 2021/day10_tests.fs
@@ -5,10 +5,36 @@ module Day10 =
   open Xunit
   open FsUnit.Xunit
 
-  [<Theory>]
-  [<InlineData("()")>]
-  let ``Day 10 - tests - valid chunks`` (input:string) =
+  // [<Theory>]
+  [<InlineData("()", "v:()")>]
+  [<InlineData("([])", "v:(v:[])")>]
+  [<InlineData("{()()()}", "v:{v:()v:()v:()}")>]
+  [<InlineData("<([{}])>", "v:<v:(v:[v:{}])>")>]
+  [<InlineData("[<>({}){}[([])<>]]", "v:[v:<>v:(v:{})v:{}v:[v:(v:[])v:<>]]")>]
+  [<InlineData("(((((((((())))))))))", "v:(v:(v:(v:(v:(v:(v:(v:(v:(v:())))))))))")>]
+  [<InlineData("(]", "c:(] expected ')' but got ']'")>]
+  [<InlineData("{()()()>", "c:{v:()v:()v:()> expected '}' but got '>'")>]
+  [<InlineData("(((()))}", "c:(v:(v:(v:()))} expected ')' but got '}'")>]
+  [<InlineData("<([]){()}[{}])", "c:<v:(v:[])v:{v:()}v:[v:{}] expected '>' but got ')'")>]
+  let ``Day 10 - tests - parse chunks`` (input:string, expected:string) =
     let r = NavigationParser.parseChunk input
 
-    r
-    |> should equal 1
+    match r with
+    | Error(e)   -> failwith e
+    | Ok(result) -> result.toAnnotatedString
+                    |> should equal expected
+
+  [<Theory>]
+  [<InlineData("()", "Valid      | v:()")>]
+  [<InlineData("[}", "Corrupted  | c:[} expected ']' but got '}'")>]
+  [<InlineData("[<>[]}", "Corrupted  | c:[v:<>v:[]} expected ']' but got '}'")>]
+  [<InlineData("[<>[]}{[]{[(<()>", "Corrupted  | c:[v:<>v:[]} expected ']' but got '}' ignoring: '{[]{[(<()>'")>]
+  [<InlineData("{([(<{}[<>[]}>{[]{[(<()>", "Corrupted  | i:{i:(i:[i:(i:<v:{}c:[v:<>v:[]} expected ']' but got '}' ignoring: '>{[]{[(<()>'")>]
+  [<InlineData("[({(<(())[]>[[{[]{<()<>>", "Incomplete | i:[i:(i:{i:(v:<v:(v:())v:[]>i:[i:[i:{v:[]i:{v:<v:()v:<>>")>]
+  let ``Day 10 - tests - parse lines`` (input:string, expected:string) =
+    let r = NavigationParser.parseLine input
+
+    match r with
+    | Error(e)   -> failwith e
+    | Ok(result) -> result.toAnnotatedString
+                    |> should equal expected
