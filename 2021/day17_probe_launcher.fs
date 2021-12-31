06963d8facd7bd9ac2558abf27bec692887a68b3
David Alpert <david@spinthemoose.com>
2021-12-30 19:26:49 -0600

    day 17 - part 1 - solved

--- 2021/day17_probe_launcher.fs
+++ 2021/day17_probe_launcher.fs
@@ -2,22 +2,143 @@ namespace AdventOfCode
 
 module ProbeLauncher =
 
-  type Velocity = { x : int ; y : int }
-
   type Position = { x : int ; y : int }
   with
     member p.isPast (area:TargetArea) =
-      p.x > area.p2.x && p.y < area.p1.y
+      p.x > area.p2.x || p.y < area.p1.y
+
+    static member initial = { x = 0; y = 0; }
+
+  and Velocity = { vx : int ; vy : int }
+
+  and Trajectory = (Position * Velocity) list
 
   and TargetArea = {
     p1: Position
     p2: Position
   }
   with
+    member area.lowestDepth = min (area.p1.y) (area.p2.y)
+    member area.greatestHeight = max (area.p1.y) (area.p2.y)
+    member area.closestEdge = area.p1.x
+    member area.farthestEdge = area.p2.x
+
     member area.contains (p:Position) =
       area.p1.x <= p.x && p.x <= area.p2.x &&
       area.p1.y <= p.y && p.y <= area.p2.y
-    
+
+    member area.mapTrajectory (vInitial:Velocity) : Trajectory =
+      let next (p:Position) (v:Velocity) =
+        let pNext = {
+          x = p.x + v.vx; // The probe's x position increases by its x velocity.
+          y = p.y + v.vy; // The probe's y position increases by its y velocity.
+        }
+
+        let vNext = {
+          // Due to drag, the probe's x velocity changes by 1 toward the value 0; that is...
+          vx = match sign (v.vx) with
+               |  1 -> v.vx - 1 // ...it decreases by 1 if it is greater than 0,
+               | -1 -> v.vy + 1 // ...increases by 1 if it is less than 0, or
+               |  0 -> 0        // ...does not change if it is already 0.
+               |  _ -> failwith "invalid sign"
+          vy = v.vy - 1; // Due to gravity, the probe's y velocity decreases by 1. 
+        }
+
+        (pNext, vNext)
+
+      // collect all the points until in the trench or past it
+      
+      Seq.unfold (fun (p:Position, v:Velocity) ->
+        if p.isPast area then
+          None
+        else
+          Some((p,v), (next p v))
+      ) (Position.initial, vInitial)
+      |> List.ofSeq
+
+    member area.endsInside (trajectory:Trajectory) =
+      let pFinal =
+        trajectory
+        |> Seq.last
+        |> fst
+
+      let inside = area.contains pFinal
+
+      // if inside then
+      //   let (_,vInitial) = trajectory.Head
+      //   printfn "%d,%d ends inside (%A)" vInitial.vx vInitial.vy inside
+
+      inside
+
+    member area.allPossibleVelocitiesLessThan (initialVY:int) =
+      Seq.unfold (fun (v:Velocity) ->
+        if (v.vy < area.greatestHeight && v.vx > area.farthestEdge) then
+          None
+        else
+          let vNext =
+            if v.vy < area.greatestHeight then
+              {
+                vx = v.vx + 1;
+                vy = initialVY;
+              }
+            else
+              {
+                vx = v.vx;
+                vy = v.vy - 1;
+              }
+
+          // printfn "v: %d,%d" v.vx v.vy
+          Some(v, vNext)
+      ) { vx = 0; vy = initialVY; }
+
+    member area.render (vInitial:Velocity) =
+      let trajectory = vInitial |> area.mapTrajectory |> List.map fst
+      let yy = trajectory |> List.map (fun p -> p.y)
+      let maxHeight = max 0 (max (yy |> List.max) (area.greatestHeight))
+      let maxDepth = min (yy |> List.min) (area.lowestDepth)
+
+      let txt =
+        seq { maxHeight .. -1 .. maxDepth }
+        |> Seq.map (fun y ->
+          seq { 0 .. area.farthestEdge }
+          |> Seq.map (fun x ->
+            let p = { x = x; y = y; }
+            if p = Position.initial then
+              "S"
+            else if trajectory |> Seq.exists (fun q -> q = p) then
+              "#"
+            else if area.contains p then
+              "T"
+            else 
+              "."
+          )
+          |> String.concat ""
+        )
+        |> String.concat "\n"
+
+      sprintf "\n%s\n" txt
+
+  let initialVelocity (trajectory:Trajectory) =
+    let _,vInitial = trajectory.Head
+    vInitial
+
+  let highestPoint (trajectory:Trajectory) =
+    let vInitial = trajectory |> initialVelocity
+
+    let highest =
+      trajectory
+      |> Seq.maxBy (fun (p,_) -> p.y)
+
+    let (p,_) = highest
+
+    // printfn "%d,%d highest: %d" vInitial.vx vInitial.vy p.y
+
+    highest
+
+  let greatestHeight (trajectory:Trajectory) =
+    let (p,_) = trajectory |> highestPoint
+    p.y
+
 module TargetAreaParser =
     open FParsec
     open FParsec.Pipes
--- 2021/day17_tests.fs
+++ 2021/day17_tests.fs
@@ -27,18 +27,20 @@ module Day17 =
   [<Theory>]
   [<InlineData("target area: x=20..30, y=-10..-5",  0,  0, false)>]
   [<InlineData("target area: x=20..30, y=-10..-5", 20, -5, true)>]
-  let ``Day 17 - tests - area contains position`` (input, x, y, expectedInside) =
+  [<InlineData("target area: x=20..30, y=-10..-5", 28, -7, true)>]
+  let ``Day 17 - tests - area contains position`` (input, x, y, expectedContains) =
     let r = parse input
     let p = { x = x; y = y; }
 
     match r with
     | Error(e)   -> failwith e
     | Ok(area) ->
-      (area.contains p) |> should equal expectedInside
+      (area.contains p) |> should equal expectedContains
 
   [<Theory>]
   [<InlineData("target area: x=20..30, y=-10..-5", 17,  -4, false)>]
-  [<InlineData("target area: x=20..30, y=-10..-5", 33,  -9, false)>]
+  [<InlineData("target area: x=20..30, y=-10..-5", 28,  -7, false)>]
+  [<InlineData("target area: x=20..30, y=-10..-5", 33,  -9, true)>]
   [<InlineData("target area: x=20..30, y=-10..-5", 48, -15, true)>]
   let ``Day 17 - tests - position is past area`` (input, x, y, expectedPast) =
     let r = parse input
@@ -48,3 +50,151 @@ module Day17 =
     | Error(e)   -> failwith e
     | Ok(area) ->
       (p.isPast area) |> should equal expectedPast
+
+  [<Theory>]
+  [<InlineData("target area: x=20..30, y=-10..-5", 0,  10, false)>]
+  [<InlineData("target area: x=20..30, y=-10..-5", 7,  2, true)>]
+  [<InlineData("target area: x=20..30, y=-10..-5", 6, 3, true)>]
+  [<InlineData("target area: x=20..30, y=-10..-5", 6, 9, true)>]
+  let ``Day 17 - tests - ends inside`` (input, x, y, expectedInside) =
+    let r = parse input
+    let v = { vx = x; vy = y; }
+
+    match r with
+    | Error(e)   -> failwith e
+    | Ok(area) ->
+      (v |> area.mapTrajectory |> area.endsInside) |> should equal expectedInside
+
+  [<Fact>]
+  let ``Day 17 - tests - map a trajectory`` () =
+    let input = "target area: x=20..30, y=-10..-5"
+    let vInitial = { vx = 7; vy = 2 }
+    let expectedTrajectory = [
+      { x =  0; y =  0 };
+      { x =  7; y =  2 };
+      { x = 13; y =  3 };
+      { x = 18; y =  3 };
+      { x = 22; y =  2 };
+      { x = 25; y =  0 };
+      { x = 27; y = -3 };
+      { x = 28; y = -7 }
+    ]
+
+    let r = parse input
+
+    match r with
+    | Error(e)   -> failwith e
+    | Ok(area) ->
+      (area.mapTrajectory vInitial)
+      |> List.map fst
+      |> should equal expectedTrajectory
+
+// 0         1         2         3
+// 0123456789012345678901234567890
+// .............#....#............   3
+// .......#..............#........   2
+// ...............................   1
+// S........................#.....   0
+// ...............................  -1
+// ...............................  -2
+// ...........................#...  -3
+// ...............................  -4
+// ....................TTTTTTTTTTT  -5
+// ....................TTTTTTTTTTT  -6
+// ....................TTTTTTTT#TT  -7
+// ....................TTTTTTTTTTT  -8
+// ....................TTTTTTTTTTT  -9
+// ....................TTTTTTTTTTT -10
+
+  [<Fact>]
+  let ``Day 17 - tests - render trajectory 7,2`` () =
+
+    let input = "target area: x=20..30, y=-10..-5"
+    let vInitial = { vx = 7; vy = 2 }
+    let expectedRender = """
+.............#....#............
+.......#..............#........
+...............................
+S........................#.....
+...............................
+...............................
+...........................#...
+...............................
+....................TTTTTTTTTTT
+....................TTTTTTTTTTT
+....................TTTTTTTT#TT
+....................TTTTTTTTTTT
+....................TTTTTTTTTTT
+....................TTTTTTTTTTT
+"""
+
+    let r = parse input
+
+    match r with
+    | Error(e)   -> failwith e
+    | Ok(area) ->
+      (area.render vInitial) |> should equal expectedRender
+
+  [<Fact>]
+  let ``Day 17 - tests - render trajectory 6,3`` () =
+
+    let input = "target area: x=20..30, y=-10..-5"
+    let vInitial = { vx = 6; vy = 3 }
+    let expectedRender = """
+...............#..#............
+...........#........#..........
+...............................
+......#..............#.........
+...............................
+...............................
+S....................#.........
+...............................
+...............................
+...............................
+.....................#.........
+....................TTTTTTTTTTT
+....................TTTTTTTTTTT
+....................TTTTTTTTTTT
+....................TTTTTTTTTTT
+....................T#TTTTTTTTT
+....................TTTTTTTTTTT
+"""
+
+    let r = parse input
+
+    match r with
+    | Error(e)   -> failwith e
+    | Ok(area) ->
+      (area.render vInitial) |> should equal expectedRender
+
+  [<Fact(Skip="silenced")>]
+  let ``Day 17 - tests - allPossibleVelocitiesLessThan`` () =
+    let input = "target area: x=20..30, y=-10..-5"
+    let r = parse input
+
+    match r with
+    | Error(e) -> failwith e
+    | Ok(area) ->
+
+      area.allPossibleVelocitiesLessThan 9
+      |> Seq.map area.mapTrajectory
+      |> Seq.filter area.endsInside
+      |> Seq.maxBy highestPoint
+      |> greatestHeight
+      |> should equal 45 
+
+  [<Fact>]
+  let ``Day 17 - part 1 - calculation`` () =
+    let r = parse day17input
+
+    match r with
+    | Error(e) -> failwith e
+    | Ok(area) ->
+
+      area.allPossibleVelocitiesLessThan 300
+      |> Seq.map area.mapTrajectory
+      |> Seq.filter area.endsInside
+      // |> Seq.filter (fun t -> (t |> greatestHeight) > 0 )
+      |> Seq.maxBy highestPoint
+      |> greatestHeight
+      |> should equal 5995 
