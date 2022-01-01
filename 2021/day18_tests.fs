c5d3e69e687fa20904756489326c2af6ff7722fb
David Alpert <david@spinthemoose.com>
2022-01-01 13:46:40 -0600

    day 18 - part 1 - sample

--- 2021/day18_tests.fs
+++ 2021/day18_tests.fs
@@ -169,3 +169,33 @@ module Day18 =
     let pair = input |> mustParse
 
     pair.magnitude |> should equal expectedMagnitude
+
+  [<Theory>]
+  [<InlineData("""
+[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+[[[5,[2,8]],4],[5,[[9,9],0]]]
+[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+[[[[5,4],[7,7]],8],[[8,3],8]]
+[[9,3],[[9,9],[6,[4,9]]]]
+[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]
+""", """
+[[[[6,6],[7,6]],[[7,7],[7,0]]],[[[7,7],[7,7]],[[7,8],[9,9]]]]
+""", 4140)>]
+
+  let ``Day 18 - part 1 - sample`` (input:string, expectedSum:string, expectedMagnitude:int64) =
+
+    let pairs = input.Trim().Split("\n")
+                |> Array.map mustParse
+                |> List.ofArray
+
+    let result =
+      pairs.Tail
+      |> List.fold (fun (s:Pair) (p:Pair) -> s + p) (pairs.Head)
+
+    result.toString |> should equal (expectedSum.Trim())
+
+    result.magnitude |> should equal expectedMagnitude
