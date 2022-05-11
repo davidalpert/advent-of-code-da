b8b60663c0e45673e48ebb8cc4a70430e2732370
David Alpert <david@spinthemoose.com>
2022-05-11 18:41:58 -0500

    2015 - day 3 - part 2

--- 2015/day03_gift_delivery.fs
+++ 2015/day03_gift_delivery.fs
@@ -36,3 +36,56 @@ module GiftDelivery =
       |> Seq.fold folder initialLocation
     
     dict
+  
+  type SantaTracker = {
+    Santa : Location
+    RoboDog : Location
+  }
+
+  let deliveryInstructionsToGiftsPerLocationWithRoboDog (input:string) =
+    let initialLocation = { x = 0; y = 0 }
+    let tracker = {
+      Santa = initialLocation;
+      RoboDog = initialLocation;
+    }
+    let dict = new Dictionary<Location, int>()
+
+    let visit (l:Location) =
+      match dict.ContainsKey(l) with
+      | true -> dict.[l] <- dict[l] + 1
+      | false -> dict.Add(l, 1)
+
+    visit (tracker.Santa)
+    visit (tracker.RoboDog)
+    // Santa and Robo-Santa start at the same location (delivering two presents to the same starting house)
+
+    let nextLocation (l:Location) (c:char) =
+      match c with
+      | '^' -> { l with y = l.y + 1 }
+      | 'v' -> { l with y = l.y - 1 }
+      | '>' -> { l with x = l.x + 1 }
+      | '<' -> { l with x = l.x - 1 }
+      | _ -> l
+
+    let folder (tracker:SantaTracker, stepNumber:int) (c:char) =
+
+      let santaMovesNext = stepNumber % 2 = 0
+
+      let nextTracker =
+        match santaMovesNext with
+        | true -> { tracker with Santa = (c |> nextLocation(tracker.Santa)) }
+        | false -> { tracker with RoboDog = (c |> nextLocation(tracker.RoboDog)) }
+        
+      if nextTracker.Santa <> tracker.Santa then
+        visit (nextTracker.Santa)
+
+      if nextTracker.RoboDog <> tracker.RoboDog then
+        visit (nextTracker.RoboDog)
+
+      (nextTracker, stepNumber + 1)
+
+    let finalLocation =
+      input
+      |> Seq.fold folder (tracker, 0)
+    
+    dict
--- 2015/day03_tests.fs
+++ 2015/day03_tests.fs
@@ -27,3 +27,24 @@ module Day03 =
 
     visitedLocations.Count
     |> should equal 2081
+
+  [<Theory>]
+  [<InlineData("^v", 3)>]
+  [<InlineData("^>v<", 3)>]
+  [<InlineData("^v^v^v^v^v", 11)>]
+  let ``Day 3 - part 2 - tests - how many houses get a single gift`` (input:string, expectedN:int) =
+    let visitedLocations =
+      input
+      |> deliveryInstructionsToGiftsPerLocationWithRoboDog
+
+    visitedLocations.Count
+    |> should equal expectedN
+
+  [<Fact>]
+  let ``Day 3 - part 2 - calculation - how many houses get a single gift`` () =
+    let visitedLocations =
+      day03input
+      |> deliveryInstructionsToGiftsPerLocationWithRoboDog
+
+    visitedLocations.Count
+    |> should equal 2341
