61cf767c07f9933e155016ca957b268aea549e40
David Alpert <david@spinthemoose.com>
2022-05-14 08:13:51 -0500

    feat: wip

--- 2015/day07_tests.fs
+++ 2015/day07_tests.fs
@@ -8,28 +8,63 @@ module Day07 =
   open Xunit
   open FsUnit.Xunit
 
-  let dictToList<'a,'b> (d:IDictionary<'a,'b>) =
-    d
-    |> Seq.map (fun pair -> (pair.Key, pair.Value))
-    |> List.ofSeq
+  // let dictToList<'a,'b> (d:IDictionary<'a,'b>) =
+  //   d
+  //   |> Seq.map (fun pair -> (pair.Key, pair.Value))
+  //   |> List.ofSeq
 
-  let expectedMapToSortedDictionary (s:string) =
-    let expectedMap = SortedDictionary<string,uint16>()
+  // let expectedMapToSortedDictionary (s:string) =
+  //   let expectedMap = SortedDictionary<string,uint16>()
 
-    s
-    |> splitToTrimmedLines
-    |> Seq.map (fun x ->
-      let parts = x.Split(":")
-      (parts.[0],(parts.[1] |> uint16))
-    )
-    |> Seq.iter (fun (c,i) -> expectedMap.Add(c,i) )
+  //   s
+  //   |> splitToTrimmedLines
+  //   |> Seq.map (fun x ->
+  //     let parts = x.Split(":")
+  //     (parts.[0],(parts.[1] |> uint16))
+  //   )
+  //   |> Seq.iter (fun (c,i) -> expectedMap.Add(c,i) )
 
-    expectedMap
+  //   expectedMap
+
+//   [<Theory>]
+//   [<InlineData("123 -> x", "x:123")>] 
+//   [<InlineData("123 -> x\nx LSHIFT 2 -> f", "f:492\nx:123")>] 
+//   [<InlineData("""
+// 123 -> x
+// 456 -> y
+// x AND y -> d
+// x OR y -> e
+// x LSHIFT 2 -> f
+// y RSHIFT 2 -> g
+// NOT x -> h
+// NOT y -> i""", """d: 72
+// e: 507
+// f: 492
+// g: 114
+// h: 65412
+// i: 65079
+// x: 123
+// y: 456""")>] 
+//   let ``Day 7 - part 1 - tests`` (input:string, expected:string) =
+//     let expectedCircuit = expected |> expectedMapToSortedDictionary
+
+//     input
+//     |> splitToTrimmedLines
+//     |> assembleCircuit
+//     |> dictToList
+//     |> should matchList (expectedCircuit |> dictToList)
 
   [<Theory>]
-  [<InlineData("123 -> x", "x:123")>] 
-  [<InlineData("123 -> x\nx LSHIFT 2 -> f", "f:492\nx:123")>] 
-  [<InlineData("""
+  [<InlineData("d", 72)>]
+  [<InlineData("e", 507)>]
+  [<InlineData("f", 492)>]
+  [<InlineData("g", 114)>]
+  [<InlineData("h", 65412)>]
+  [<InlineData("i", 65079)>]
+  [<InlineData("x", 123)>]
+  [<InlineData("y", 456)>]
+  let ``Day 7 - part 1 - sample`` (w:string, expected:int) =
+    let circuitMap = """
 123 -> x
 456 -> y
 x AND y -> d
@@ -37,29 +72,22 @@ x OR y -> e
 x LSHIFT 2 -> f
 y RSHIFT 2 -> g
 NOT x -> h
-NOT y -> i""", """d: 72
-e: 507
-f: 492
-g: 114
-h: 65412
-i: 65079
-x: 123
-y: 456""")>] 
-  let ``Day 7 - part 1 - tests`` (input:string, expected:string) =
-    let expectedCircuit = expected |> expectedMapToSortedDictionary
+NOT y -> i
+    """
 
-    input
+    circuitMap
     |> splitToTrimmedLines
     |> assembleCircuit
-    |> dictToList
-    |> should matchList (expectedCircuit |> dictToList)
+    |> valueSentToWire w
+    |> should equal (expected |> uint16)
 
   [<Theory>]
   [<InlineData(1,1,2)>]
   [<InlineData(1,2,4)>]
   [<InlineData(123,2,492)>]
   let ``bitwise <<< test``(x,y,e) =
-    x <<< y |> should equal e
+    x <<< y
+    |> should equal e
 
   [<Fact>]
   let ``Day 7 - part 1 - calculation``() =
@@ -68,4 +96,4 @@ y: 456""")>]
       |> splitToTrimmedLines
       |> assembleCircuit
 
-    circuitMap.["a"] |> should equal -1
+    circuitMap.["a"].valueOf(circuitMap) |> should equal -1
