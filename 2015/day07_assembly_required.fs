35bd99f31407af908e5ca67cf4869063e7c51bc7
David Alpert <david@spinthemoose.com>
2022-05-14 08:13:51 -0500

    2015 - day 7 - part 1 - spike #2
    building the logic gate in memory and evaluating
    by recursion and pattern matching

--- 2015/day07_assembly_required.fs
+++ 2015/day07_assembly_required.fs
@@ -8,25 +8,61 @@ module AssemblyRequired =
   open System
   open System.Collections.Generic
 
-  let assembleCircuit (instructions:seq<string>) =
-    let cc = SortedDictionary<string,uint16>()
+  type wire = string
 
-    let safeGetValue (c:string) =
-      if cc.ContainsKey(c) then
-        // printfn "getting value of '%s': %d" c (cc.[c])
-        cc.[c]
-      else
-        0 |> uint16
+  type CircuitComponent =
+  | Signal of uint16 * wire
+  | DirectRoutingFrom of wire * wire
+  | BitwiseAnd of wire * wire * wire
+  | BitwiseNumericAnd of uint16 * wire * wire
+  | BitwiseOr of wire * wire * wire
+  | LeftShift of wire * int * wire
+  | RightShift of wire * int * wire
+  | BitwiseComplement of wire * wire
+  with
+    static member cache = Dictionary<wire,uint16>()
+
+    member this.valueOf (cc:IDictionary<wire,CircuitComponent>) =
+      // printfn "valueOf: %A" this
+      let cacheOrLookup (w:wire) =
+        if CircuitComponent.cache.ContainsKey(w) then
+          printfn "cachedValueOf %A" w 
+          CircuitComponent.cache.[w]
+        else
+          let v = (cc.[w].valueOf cc)
+          CircuitComponent.cache.Add(w,v)
+          v
+
+      match this with
+      | Signal(v,w) -> v
+      | DirectRoutingFrom(x,w) -> cacheOrLookup x
+      | BitwiseAnd(x,y,w) -> (cacheOrLookup x) &&& (cacheOrLookup y)
+      | BitwiseNumericAnd(x,y,w) -> x &&& (cacheOrLookup y)
+      | BitwiseOr(x,y,w) -> (cacheOrLookup x) ||| (cacheOrLookup y)
+      | LeftShift(x,v,w) -> (cacheOrLookup x) <<< v
+      | RightShift(x,v,w) -> (cacheOrLookup x) >>> v
+      | BitwiseComplement(x,w) -> ~~~ (cacheOrLookup x)
+
+  let valueSentToWire (w:string) (cc:IDictionary<wire,CircuitComponent>) =
+    if cc.ContainsKey(w) then
+      cc.[w].valueOf cc
+    else
+      0 |> uint16
+
+  let assembleCircuit (instructions:seq<string>) =
+    let cc = SortedDictionary<wire,CircuitComponent>()
 
     instructions
     |> Seq.iter (fun i ->
       match i with
-      | Regex @"^(\d+) -> (\w)" [ v; c] -> cc.[c] <- (v |> uint16)
-      | Regex @"^(\w+) AND (\w+) -> (\w+)" [ x; y; d] -> cc.[d] <- ((x |> safeGetValue) &&& (y |> safeGetValue))
-      | Regex @"^(\w+) OR (\w+) -> (\w+)" [ x; y; d] -> cc.[d] <- ((x |> safeGetValue) ||| (y |> safeGetValue))
-      | Regex @"^(\w+) LSHIFT (\d+) -> (\w+)" [ c; v; d] -> cc.[d] <- ((c |> safeGetValue) <<< (v |> int))
-      | Regex @"^(\w+) RSHIFT (\d+) -> (\w+)" [ c; v; d] -> cc.[d] <- ((c |> safeGetValue) >>> (v |> int))
-      | Regex @"^NOT (\w+) -> (\w+)" [ x; d ] -> cc.[d] <- (~~~ (x |> safeGetValue))
+      | Regex @"^(\d+) -> (\w+)" [ v; w] -> cc.[w] <- Signal(v |> uint16,w)
+      | Regex @"^(\w+) -> (\w+)" [ x; w] -> cc.[w] <- DirectRoutingFrom(x,w)
+      | Regex @"^(\d+) AND (\w+) -> (\w+)" [ x; y; w] -> cc.[w] <- BitwiseNumericAnd(x |> uint16,y,w)
+      | Regex @"^(\w+) AND (\w+) -> (\w+)" [ x; y; w] -> cc.[w] <- BitwiseAnd(x,y,w)
+      | Regex @"^(\w+) OR (\w+) -> (\w+)" [ x; y; w] -> cc.[w] <- BitwiseOr(x,y,w)
+      | Regex @"^(\w+) LSHIFT (\d+) -> (\w+)" [ x; v; w] -> cc.[w] <- LeftShift(x,v |> int,w)
+      | Regex @"^(\w+) RSHIFT (\d+) -> (\w+)" [ x; v; w] -> cc.[w] <- RightShift(x,v |> int,w)
+      | Regex @"^NOT (\w+) -> (\w+)" [ x; w ] -> cc.[w] <- BitwiseComplement(x,w)
       | _ -> printfn "ignoring: %s" i
     )
 
