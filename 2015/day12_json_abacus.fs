18515a7e49216b1af863cf150bb023e67a75ce3f
David Alpert <david@spinthemoose.com>
2022-05-15 13:25:32 -0500

    2015 - day 12 - part 2 (thank you fsharp)

--- 2015/day12_json_abacus.fs
+++ 2015/day12_json_abacus.fs
@@ -20,3 +20,29 @@ module JsonAbacus =
         match parseJsonString s with
         | Success (v, _, _) -> calculateSum v
         | Failure (msg, err, _) -> raise (System.Exception(msg))
+
+    let sumOfAllNumbers2 (s: string) : float =
+
+        let isRed (node: Json) : bool =
+            match node with
+            | JObject (m) ->
+                (m
+                 |> Seq.tryFind (fun pair -> pair.Value = JString("red")))
+                    .IsSome
+            | _ -> false
+
+        let rec calculateSum (node: Json) =
+            if isRed node then
+                0 |> float
+            else
+                match node with
+                | JNumber (f) -> f
+                | JList (l) -> l |> List.sumBy calculateSum
+                | JObject (m) ->
+                    m
+                    |> Seq.sumBy (fun pair -> calculateSum (pair.Value))
+                | _ -> 0
+
+        match parseJsonString s with
+        | Success (v, _, _) -> calculateSum v
+        | Failure (msg, err, _) -> raise (System.Exception(msg))
--- 2015/day12_tests.fs
+++ 2015/day12_tests.fs
@@ -26,3 +26,26 @@ module Day12 =
         day12input
         |> sumOfAllNumbers
         |> should equal (191164 |> float)
+
+    [<Theory>]
+    [<InlineData("[1,2,3]", 6)>]
+    [<InlineData("""{"a":2,"b":4}""", 6)>]
+    [<InlineData("""[[[3]]]""", 3)>]
+    [<InlineData("""{"a":{"b":4},"c":-1}""", 3)>]
+    [<InlineData("""{"a":[-1,1]}""", 0)>]
+    [<InlineData("""[-1,{"a":1}]""", 0)>]
+    [<InlineData("""[]""", 0)>]
+    [<InlineData("""{}""", 0)>]
+    [<InlineData("""[1,{"c":"red","b":2},3]""", 4)>]
+    [<InlineData("""{"d":"red","e":[1,2,3,4],"f":5}""", 0)>]
+    [<InlineData("""[1,"red",5]""", 6)>]
+    let ``Day 12 - part 2 - examples`` (s, expected) =
+        s
+        |> sumOfAllNumbers2
+        |> should equal (expected |> float)
+
+    [<Fact>]
+    let ``Day 12 - part 2 - calculation`` () =
+        day12input
+        |> sumOfAllNumbers2
+        |> should equal (87842 |> float)
