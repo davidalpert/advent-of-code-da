dd8597d46cd79af328d9d9776da4b80e99193ad1
David Alpert <david@spinthemoose.com>
2022-05-15 16:58:07 -0500

    2015 - day 13 - part 1

---
+++ 2015/day13_seating_arrangements.fs
@@ -0,0 +1,76 @@
+namespace AdventOfCode
+
+module SeatingArrangements =
+
+    open AdventOfCode.utils
+
+    let parseHappinessRelationship (s: string) =
+        match s with
+        | Regex @"(\w+) would gain (\d+) happiness units by sitting next to (\w+)" [ x; d; y ] -> ((x, y), d |> int)
+        | Regex @"(\w+) would lose (\d+) happiness units by sitting next to (\w+)" [ x; d; y ] ->
+            ((x, y), (d |> int) * -1)
+        | _ -> raise (System.Exception(sprintf "could not parse: '%s'" s))
+
+    let parseInput (s: string) =
+        s.Trim().Split("\n")
+        |> Array.map (fun ss -> ss.Trim())
+        |> Array.map parseHappinessRelationship
+
+    let toImpactMap (s: string) = s |> parseInput |> Map.ofSeq
+
+    let findHappiestAggangement (s: string) =
+        let relationships = s |> parseInput
+
+        let knights =
+            relationships
+            |> Array.collect (fun ((x, y), _) -> [| x; y |])
+            |> Array.distinct
+
+        let impactMap = relationships |> Map.ofSeq
+
+        let possibilities =
+            (permuteAll (knights |> List.ofArray))
+            |> Seq.map (fun l -> l |> Array.ofList)
+
+        possibilities
+        |> Seq.map (fun arrangement ->
+            let size = arrangement.Length
+
+            let happinessDeltaPerPerson =
+                arrangement
+                |> Array.mapi (fun i name ->
+                    let leftI =
+                        (match i with
+                         | 0 -> size
+                         | _ -> i)
+                        - 1
+
+                    let rightI = (i + 1) % size
+
+                    let leftName = arrangement.[leftI]
+                    let rightName = arrangement.[rightI]
+
+                    let leftImpact = impactMap.[(name, leftName)]
+                    let rightImpact = impactMap.[(name, rightName)]
+
+                    // printfn
+                    //     "%s <(%d)< %s [%d] >(%d)> %s"
+                    //     leftName
+                    //     leftImpact
+                    //     name
+                    //     (leftImpact + rightImpact)
+                    //     rightImpact
+                    //     rightName
+
+                    leftImpact + rightImpact)
+
+            Array.zip arrangement happinessDeltaPerPerson)
+
+        |> Seq.map (fun arrangement ->
+            let (names, impacts) = arrangement |> Array.unzip
+            (names, impacts |> Array.sum))
+        |> Seq.sortByDescending (fun (_, impact) -> impact)
+        // |> Seq.map (fun r ->
+        //     printfn "%A" r
+        //     r)
+        |> Seq.maxBy (fun (_, impact) -> impact)
---
+++ 2015/day13_tests.fs
@@ -0,0 +1,48 @@
+namespace AdventOfCode.Tests
+
+module Day13 =
+
+    open AdventOfCode.Input
+    open AdventOfCode.SeatingArrangements
+    open Xunit
+    open FsUnit.Xunit
+
+    let exampleInput =
+        """
+Alice would gain 54 happiness units by sitting next to Bob.
+Alice would lose 79 happiness units by sitting next to Carol.
+Alice would lose 2 happiness units by sitting next to David.
+Bob would gain 83 happiness units by sitting next to Alice.
+Bob would lose 7 happiness units by sitting next to Carol.
+Bob would lose 63 happiness units by sitting next to David.
+Carol would lose 62 happiness units by sitting next to Alice.
+Carol would gain 60 happiness units by sitting next to Bob.
+Carol would gain 55 happiness units by sitting next to David.
+David would gain 46 happiness units by sitting next to Alice.
+David would lose 7 happiness units by sitting next to Bob.
+David would gain 41 happiness units by sitting next to Carol.
+"""
+
+    [<Theory>]
+    [<InlineData("Alice", "Bob", 54)>]
+    [<InlineData("Bob", "Alice", 83)>]
+    [<InlineData("Alice", "David", -2)>]
+    let ``Day13 - part 1 - happinessMap`` (a, b, expected) =
+        exampleInput
+        |> toImpactMap
+        |> (fun m -> m.[(a, b)])
+        |> should equal expected
+
+    [<Fact>]
+    let ``Day13 - part 1 - examples`` () =
+        exampleInput
+        |> findHappiestAggangement
+        |> snd
+        |> should equal 330
+
+    [<Fact>]
+    let ``Day13 - part 1 - calculation`` () =
+        day13input
+        |> findHappiestAggangement
+        |> snd
+        |> should equal 664
