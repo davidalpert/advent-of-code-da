79e27ce3b2a871fe2398ad92d97fdd01f13e85e4
David Alpert <david@spinthemoose.com>
2022-05-21 20:14:26 -0500

    2015 - format day00_utils

--- 2015/day00_utils.fs
+++ 2015/day00_utils.fs
@@ -2,63 +2,68 @@ namespace AdventOfCode
 
 module utils =
 
-  open System.Text.RegularExpressions
+    open System.Text.RegularExpressions
 
-  // http://www.fssnip.net/29/title/Regular-expression-active-pattern
-  let (|Regex|_|) pattern input =
-    let m = Regex.Match(input, pattern)
-    if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
-    else None
+    // http://www.fssnip.net/29/title/Regular-expression-active-pattern
+    let (|Regex|_|) pattern input =
+        let m = Regex.Match(input, pattern)
 
-  // https://rolfsuter.ch/code/f-transpose-a-list-of-lists-transpose-2d-matrix/
-  let rec transpose =
-    function
-    | (_ :: _) :: _ as M ->
-        List.map List.head M
-        :: transpose (List.map List.tail M)
-    | _ -> []
+        if m.Success then
+            Some(List.tail [ for g in m.Groups -> g.Value ])
+        else
+            None
 
-  // https://stackoverflow.com/a/1231711/8997
-  let rec comb n l = 
-    match n, l with
-    | 0, _ -> [[]]
-    | _, [] -> []
-    | k, (x::xs) -> List.map ((@) [x]) (comb (k-1) xs) @ comb k xs
+    // https://rolfsuter.ch/code/f-transpose-a-list-of-lists-transpose-2d-matrix/
+    let rec transpose =
+        function
+        | (_ :: _) :: _ as M ->
+            List.map List.head M
+            :: transpose (List.map List.tail M)
+        | _ -> []
 
-  // http://www.fssnip.net/fF/title/Combinations-n-choose-k
-  let n_choose_k k n =
-    let rec choose lo  =
-      function
-        | 0  -> [[]]
-        | i -> [for j=lo to (Array.length n)-1 do
-                                      for ks in choose (j+1) (i-1) do
-                                        yield n.[j] :: ks ]
-      in choose 0 k 
+    // https://stackoverflow.com/a/1231711/8997
+    let rec comb n l =
+        match n, l with
+        | 0, _ -> [ [] ]
+        | _, [] -> []
+        | k, (x :: xs) -> List.map ((@) [ x ]) (comb (k - 1) xs) @ comb k xs
 
-  // https://rosettacode.org/wiki/Permutations#F.23
-  let rec permuteAll sourceList =
-    let rec insert left x right = seq {
-      match right with
-      | [] -> yield left @ [x]
-      | head :: tail -> 
-          yield left @ [x] @ right
-          yield! insert (left @ [head]) x tail
-      }
- 
-    seq {
-        match sourceList with
-        | [] -> yield []
-        | head :: tail -> yield! Seq.collect (insert [] head) (permuteAll tail)
-    }
-  // // debug helpers
-  // let ( ~~~ ) =
-  //   // printfn
-  //   sprintf
+    // http://www.fssnip.net/fF/title/Combinations-n-choose-k
+    let n_choose_k k n =
+        let rec choose lo =
+            function
+            | 0 -> [ [] ]
+            | i ->
+                [ for j = lo to (Array.length n) - 1 do
+                      for ks in choose (j + 1) (i - 1) do
+                          yield n.[j] :: ks ] in choose 0 k
 
-  // let ( ~~~~ ) =
-  //   // printfn
-  //   sprintf
+    // https://rosettacode.org/wiki/Permutations#F.23
+    let rec permuteAll sourceList =
+        let rec insert left x right =
+            seq {
+                match right with
+                | [] -> yield left @ [ x ]
+                | head :: tail ->
+                    yield left @ [ x ] @ right
+                    yield! insert (left @ [ head ]) x tail
+            }
 
-  // let ( ~~~~~ ) =
-  //   // printfn
-  //   sprintf
+        seq {
+            match sourceList with
+            | [] -> yield []
+            | head :: tail -> yield! Seq.collect (insert [] head) (permuteAll tail)
+        }
+
+// // debug helpers
+// let ( ~~~ ) =
+//   // printfn
+//   sprintf
+
+// let ( ~~~~ ) =
+//   // printfn
+//   sprintf
+
+// let ( ~~~~~ ) =
+//   // printfn
+//   sprintf
