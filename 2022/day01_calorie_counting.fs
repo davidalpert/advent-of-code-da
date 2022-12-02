f46838accd54a73e56c25f47bf46dc7f8f520a07
David Alpert <david@spinthemoose.com>
2022-12-02 08:14:49 -0600

    2022: day 1

--- 2022/day01_calorie_counting.fs
+++ 2022/day01_calorie_counting.fs
@@ -8,9 +8,26 @@ module CalorieCounting =
     open AdventOfCode.utils
     open FSharp.Data.UnitSystems.SI.UnitNames
 
-    let fromInput (s: string) =
+    let inputToElvesWithCalories (s:string) =
+        s.Trim().Split("\n\n")
+        |> Array.map (fun s -> s.Split("\n"))
+        |> Array.map (fun ss -> ss
+                             |> Array.map (fun s -> s |> int))
+        |> Array.mapi (fun i items ->
+            let n = i + 1 // arrays are 0-indexed; humans count from 1
+            let calories =
+                items
+                |> Array.sum
+            (n, calories))
+        
+    let inputToElfWithMaxCalories (s: string) =
         s
-        // |> Seq.map (fun s ->
-        //     // printfn "s: %s" s
-        //     s)
-        // |> Array.ofSeq
+        |> inputToElvesWithCalories
+        |> Array.maxBy snd
+    
+    let inputToTopThreeElvesCarryingTheMostCalories (s: string) =
+        s
+        |> inputToElvesWithCalories
+        |> Array.sortByDescending snd
+        |> Array.take 3
+    
