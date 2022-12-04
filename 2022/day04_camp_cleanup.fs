91035678a0356801d59bb0bab422cb4afd39418e
David Alpert <david@spinthemoose.com>
2022-12-04 09:19:27 -0600

    2022 - day 04 - code cleanup
    refactored into smaller pieces
    
    introduced some more Kirk-inspired helpers
    (e.g. spread) to refactor duplication and
    increase composability (e.g. eitherOr)
    
    this results in more lines of code but
    each individual function is simpler

--- 2022/day04_camp_cleanup.fs
+++ 2022/day04_camp_cleanup.fs
@@ -8,6 +8,8 @@ module CampCleanup =
     open AdventOfCode.utils
     open FSharp.Data.UnitSystems.SI.UnitNames
 
+    // helpers
+    
     let firstPairFromArray ss =
         match Array.length ss with
         | l when l >= 2 -> (ss[0], ss[1])
@@ -16,34 +18,42 @@ module CampCleanup =
     let rangeAsSet (first, last) =
         seq { first .. last } |> Set.ofSeq
         
+    let eitherOr fn (a, b) =
+        (a |> fn b) || (b |> fn a)
+        
+    let spread fn (a, b) =
+        (fn a, fn b)
+    
+    let contains a b =
+        Set.isSubset a b
+        
+    let overlaps a b =
+        (Set.intersect a b) <> Set.empty
+        
+    // parsing
+    
     let parseRange (s: string) =
             s.Split("-")
             |> Array.map int
             |> firstPairFromArray
             |> rangeAsSet
         
-    let contains a b =
-        Set.isSubset a b
-        
-    let overlaps a b =
-        (Set.intersect a b) <> Set.empty
-            
     let inputToPairedAssignments s =
         s
         |> splitToTrimmedLines
         |> Seq.map (fun s -> s.Split(",") |> firstPairFromArray)
-        |> Seq.map (fun (left, right) -> (left |> parseRange, right |> parseRange))
+        |> Seq.map (spread parseRange)
         
     // part 1 ---------------
     
     let how_many_assignment_pairs_contain_each_other pairs =
         pairs
-        |> Seq.where (fun (left, right) -> (left |> contains right) || (right |> contains left))
+        |> Seq.where (eitherOr contains)
         |> Seq.length
     
     // part 2 ---------------
     
     let how_many_assignment_pairs_overlap pairs =
         pairs
-        |> Seq.where (fun (left,right) -> (left |> overlaps right) || (right |> overlaps left))
+        |> Seq.where (eitherOr overlaps)
         |> Seq.length
