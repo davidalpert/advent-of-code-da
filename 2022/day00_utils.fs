1742198b9e692eadd401410b427117994d77d0b0
David Alpert <david@spinthemoose.com>
2022-12-17 14:01:50 -0600

    refactor: extract some common utilities

--- 2022/day00_utils.fs
+++ 2022/day00_utils.fs
@@ -1,6 +1,8 @@
 namespace AdventOfCode
 
 module utils =
+  open FParsec
+  
   // https://rolfsuter.ch/code/f-transpose-a-list-of-lists-transpose-2d-matrix/
   let rec transpose =
     function
@@ -9,15 +11,37 @@ module utils =
         :: transpose (List.map List.tail M)
     | _ -> []
 
-  // debug helpers
-  let ( ~~~ ) =
-    // printfn
-    sprintf
 
-  let ( ~~~~ ) =
-    // printfn
-    sprintf
+  // trace helper to assist with debugging misbehaving parsers
+  let (<!>) (p: Parser<_,_>) (label) : Parser<_,_> =
+    fun stream ->
+      printfn "%A: Entering %s" stream.Position label
+      let reply = p stream
+      printfn "%A: Leaving %s (%A)" stream.Position label reply.Status
+      reply
 
-  let ( ~~~~~ ) =
-    // printfn
-    sprintf
+  let firstPairFromArray ss =
+    match Array.length ss with
+    | l when l >= 2 -> (ss[0], ss[1])
+    | _ -> failwithf $"firstPairFromArray requires at least 2 elements; found %d{ss |> Array.length}"
+        
+  let asSet (first, last) =
+    seq { first .. last } |> Set.ofSeq
+        
+  let eitherOr fn (a, b) =
+    (a |> fn b) || (b |> fn a)
+        
+  let spread fn (a, b) =
+    (fn a, fn b)
+    
+  let contains a b =
+    Set.isSubset a b
+        
+  let overlaps a b =
+    (Set.intersect a b) <> Set.empty
+
+  let splitBy (sep: string) (input: string) =
+    input.Split(sep)
+        
+  let joinBy (sep:string) (values:string[]) =
+     System.String.Join(sep, values)
