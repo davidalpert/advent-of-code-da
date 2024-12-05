f28c3c79f667fee017b7479817de148eaab4c7c8
David Alpert <david@spinthemoose.com>
2024-12-05 12:03:20 -0600

    2024 - Day 4 - Part 1

--- 2024/day04_ceres_search.fs
+++ 2024/day04_ceres_search.fs
@@ -9,22 +9,58 @@ module day04_Ceres_Search =
     open FParsec
     open FParsec.Pipes
 
-    module parser =
-        let ws = spaces
-        let ch = pchar
+    module part1 =
+        let needle = [|'X';'M';'A';'S'|]
 
-        let mustParse p (input:string) =
-            match run p input with
-            | Success(r, _, _) -> r
-            | Failure(errorMsg, _, _) -> failwith errorMsg
+        let surroundingPositions (x,y) =
+            [|
+                [|(x,y);(x-1,y-1);(x-2,y-2);(x-3,y-3)|]; [|(x,y);(x,y-1);(x,y-2);(x,y-3)|]; [|(x,y);(x+1,y-1);(x+2,y-2);(x+3,y-3)|];
+                [|(x,y);(x-1,y);(x-2,y);(x-3,y)|];                                          [|(x,y);(x+1,y);(x+2,y);(x+3,y)|];
+                [|(x,y);(x-1,y+1);(x-2,y+2);(x-3,y+3)|]; [|(x,y);(x,y+1);(x,y+2);(x,y+3)|]; [|(x,y);(x+1,y+1);(x+2,y+2);(x+3,y+3)|];
+            |]
 
-        let parseInput (input:string) =
-            mustParse ws input
+        type Pos = { x:int; y:int }
+        with
+            override this.ToString() = $"(%d{this.x},%d{this.y})"
+            
+            static member fromTuple (p:int*int) = { x = fst p; y = snd p }
+
+            member this.surroundingSearchPaths =
+                surroundingPositions (this.x, this.y)
+                |> Array.map (fun path -> path |> Array.map Pos.fromTuple)
+
+        let findInstances (searchGrid:char array array) =
+            let maxX = searchGrid.Length - 1
+            let maxY = searchGrid.[0].Length - 1
+            seq {
+                for x in 0..maxX do
+                    for y in 0..maxY do
+                        yield { x = x; y = y }
+            }
+            |> Seq.collect (fun pos ->
+                pos.surroundingSearchPaths
+                |> Seq.map (fun path ->
+                    let pathMatches = needle |> Array.mapi (fun i c ->
+                        let p = path.[i]
+                        if p.x < 0 || p.x > maxX || p.y < 0 || p.y > maxY then
+                            None
+                        else
+                            if searchGrid.[p.x].[p.y] = c then
+                                Some(c)
+                            else
+                                None
+                    )
+                    if pathMatches |> Array.forall Option.isSome then
+                        Some(pos, path)
+                    else
+                        None
+                )
+                |> Seq.filter Option.isSome |> Array.ofSeq
+            )
+            |> Array.ofSeq
 
     let fromInput (s: string) =
         s
         |> splitToTrimmedLines
-        |> Seq.map (fun s ->
-            // printfn "s: %s" s
-            s)
+        |> Seq.map (_.ToCharArray())
         |> Array.ofSeq
--- 2024/day04_tests.fs
+++ 2024/day04_tests.fs
@@ -10,20 +10,34 @@ module Day04 =
 
     let exampleInput =
         """
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
 """
 
     [<Fact>]
     let ``2024 - Day 04 - part 1 - example`` () =
         exampleInput
-        |> parser.parseInput
-        |> should equal null
+        |> fromInput
+        |> part1.findInstances
+        |> Array.length
+        |> should equal 18
 
-    // [<Fact>]
+    [<Fact>]
     let ``2024 - Day 04 - part 1`` () =
         day04input
-        // |> fromInput
-        // |> Array.length
-        |> printfn "2024 - Day 04 - Part 1: %A"
+        |> fromInput
+        |> part1.findInstances
+        |> Array.length
+        |> should equal 2406
+        // |> printfn "2024 - Day 04 - Part 1: %A"
 
     // [<Fact>]
     let ``2024 - Day 04 - part 2 - example`` () =
